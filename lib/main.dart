2948fab00a5ff489c3d80c3a602b14189ee6ab77
komachihashimoto <joujinameko@gmail.com>
2024-01-29 15:30:57 +0900

    ゲームのロジック

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   runApp(const MyApp());
@@ -24,7 +25,6 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
-
   final String title;
 
   @override
@@ -33,10 +33,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
+  int _time = 0;
+  int _randomCell = Random().nextInt(9);
+  int _previousCell = -1;
 
   void _incrementCounter() {
     setState(() {
       _counter++;
+      do {
+        _randomCell = Random().nextInt(9);
+      } while (_randomCell == _previousCell);
+      _previousCell = _randomCell;
     });
   }
 
@@ -54,31 +61,32 @@ class _MyHomePageState extends State<MyHomePage> {
       body: Column(
         children: <Widget>[
           Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Text(
+              'Time: $_time',
+              style: Theme.of(context).textTheme.headline3?.copyWith(color: Colors.white) ?? TextStyle(color: Colors.white, fontSize: 30),
+            ),
+          ),
+          Padding(
             padding: const EdgeInsets.all(60.0),
             child: Text(
-              'Count: $_counter',
-              style: Theme
-                  .of(context)
-                  .textTheme
-                  .headline4
-                  ?.copyWith(color: Colors.white) ??
-                  TextStyle(color: Colors.white),
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4?.copyWith(color: Colors.white) ?? TextStyle(color: Colors.white),
             ),
           ),
           Expanded(
             child: GridView.count(
               crossAxisCount: 3,
               children: List.generate(9, (index) {
-                return Container(
-                  key: ValueKey('cell_$index'),
-                  decoration: BoxDecoration(
-                    border: Border.all(color: Colors.white),
-                    color: Colors.grey[850],
-                  ),
-                  child: Center(
-                    child: Text(
-                      '$index',
-                      style: TextStyle(color: Colors.white),
+                return GestureDetector(
+                  onTap: index == _randomCell ? _incrementCounter : null,
+                  child: Container(
+                    key: ValueKey('cell_$index'),
+                    decoration: BoxDecoration(
+                      border: Border.all(color: Colors.white),
+                      color: index == _randomCell ? Colors.white : Colors.grey[850],
+                    ),
+                    child: Center(
                     ),
                   ),
                 );
@@ -90,5 +98,3 @@ class _MyHomePageState extends State<MyHomePage> {
     );
   }
 }
-
-
